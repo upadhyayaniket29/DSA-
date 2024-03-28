38683909b7c423d1822dcf1e4ae652a755769fb7
Aniket Upadhyay <88603347+upadhyayaniket29@users.noreply.github.com>
2024-03-28 05:21:24 +0000

    DSA

---
+++ Hello world.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+
+    cout<<"hello world";
+    cout<<"hello my name is aniket upadhyay";
+    cin>>a;
+    cout<<a;
+
+return 0;
+}
